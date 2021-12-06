0ddfe72c213b67abe93b422fe07c635b87fb6cdd
Rohochiy <oxid1109@gmail.com>
2021-12-06 16:08:04 +0200

    task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,19 @@
-// let credits = 35500;
-// let pricePerDroid = 3000;
-// let totalPrice = 0;
+let credits = 35500;
+let pricePerDroid = 3000;
+let totalPrice;
 
-// let numberDroid = prompt("Сколько диодов вы хотите купить?");
+let number = prompt("Сколько дроидов вы хотите купить?");
+if (number === null) {
+  message = "Отменено пользователем!";
+} else {
+  totalPrice = number * pricePerDroid;
+  if (totalPrice > credits) {
+    message = "Недостаточно средств на счету!";
+  }
+}
+if (totalPrice <= credits) {
+  message = `Вы купили ${number} дроидов, на счету осталось ${
+    credits - totalPrice
+  } кредитов.`;
+}
+alert(message);
