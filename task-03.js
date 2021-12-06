9218f5f105f367deb025a30406b105861095003e
Rohochiy <oxid1109@gmail.com>
2021-12-06 12:15:07 +0200

    task-3

---
+++ task-03.js
@@ -0,0 +1,12 @@
+const ADMIN_PASSWORD = "adminpass";
+let message;
+let input = prompt("Введите пароль");
+if (input === null) {
+  message = "Отменено пользователем!";
+} else if (input === ADMIN_PASSWORD) {
+  message = "Добро пожаловать!";
+} else {
+  message = "Доступ запрещен, неверный пароль!";
+}
+
+alert(message);
